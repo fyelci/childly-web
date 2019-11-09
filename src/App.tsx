ec96c51b4ec9fe9529adb6c0d7c68e5b02287ba2
fyelci <fyelci@gmail.com>
2019-11-09 22:35:57 +0000

    Add Chat detail and navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Route,
+  Redirect,
+  RouteComponentProps,
+} from 'react-router-dom';
+import ChatRoomScreen from './components/ChatRoomScreen';
 import ChatsListScreen from './components/ChatsListScreen';
+import AnimatedSwitch from './components/AnimatedSwitch';
 
 const App: React.FC = () => {
+  const redirectToChats = () => <Redirect to="/chats" />;
+
   return (
-    <div>
-      <ChatsListScreen />
-    </div>
+    <BrowserRouter>
+      <AnimatedSwitch>
+        <Route exact path="/chats" component={ChatsListScreen} />
+        <Route
+          exact
+          path="/chats/:chatId"
+          component={({
+            match,
+            history,
+          }: RouteComponentProps<{ chatId: string }>) => (
+            <ChatRoomScreen chatId={match.params.chatId} history={history} />
+          )}
+        />
+      </AnimatedSwitch>
+      <Route exact path="/" render={redirectToChats} />
+    </BrowserRouter>
   );
 };
 
--- src/components/ChatsListScreen/ChatsList.stories.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-import { storiesOf } from '@storybook/react';
-import ChatsList from './ChatsList';
-
-storiesOf('ChatsListScreen', module)
-  .add('First Story', () => <ChatsList />)
-  .add('Second story', () => <ChatsList />);
